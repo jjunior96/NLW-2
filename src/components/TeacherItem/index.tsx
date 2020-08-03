5fa1bd5dd7e52b9b26843e810420125c0c7e57ef
Junior <jjalves96@gmail.com>
2020-08-03 11:53:44 -0300

    feat: TeacherItem component

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+
+import "./styles.css";
+
+function TeacherItem() {
+  return (
+    <article className='teacher-item'>
+      <header>
+        <img
+          src='https://avatars0.githubusercontent.com/u/30422190?s=460&u=98382289c345c9ac341e5ecbb74ae6eebe44e3ac&v=4'
+          alt='Junior Alves'
+        />
+        <div>
+          <strong>Junior Alves</strong>
+          <span>Matemática</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta em React
+        <br />
+        <br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis eum ut
+        quam fugiat obcaecati temporibus.
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 100,00</strong>
+        </p>
+        <button type='button'>
+          <img src={whatsappIcon} alt='Whatsapp' />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+}
+
+export default TeacherItem;
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 
 import PageHeader from "../../components/PageHeader";
-
-import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+import TeacherItem from "../../components/TeacherItem";
 
 import "./styles.css";
 
@@ -29,37 +28,11 @@ function TeacherList() {
       </PageHeader>
 
       <main>
-        <article className='teacher-item'>
-          <header>
-            <img
-              src='https://avatars0.githubusercontent.com/u/30422190?s=460&u=98382289c345c9ac341e5ecbb74ae6eebe44e3ac&v=4'
-              alt='Junior Alves'
-            />
-            <div>
-              <strong>Junior Alves</strong>
-              <span>Matemática</span>
-            </div>
-          </header>
-
-          <p>
-            Entusiasta em React
-            <br />
-            <br />
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
-            eum ut quam fugiat obcaecati temporibus.
-          </p>
-
-          <footer>
-            <p>
-              Preço/hora
-              <strong>R$ 100,00</strong>
-            </p>
-            <button type='button'>
-              <img src={whatsappIcon} alt='Whatsapp' />
-              Entrar em contato
-            </button>
-          </footer>
-        </article>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
       </main>
     </div>
   );

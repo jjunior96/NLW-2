561f8ab527897a15581afa7cb9be9b3f6024c2db
Junior <jjalves96@gmail.com>
2020-08-04 15:53:24 -0300

    feat: created route GET classes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,61 +1,10 @@
 import express from 'express';
-import database from './database/connection';
-import convertHoursToMinutes from './utils/convertHoursToMinutes';
+import ClassesController from './controllers/ClassesController';
 
 const routes = express.Router();
+const classesController = new ClassesController();
 
-interface ScheduleItem {
-  week_day: number;
-  from: string;
-  to: string;
-}
-
-routes.post('/classes', async (request, response) => {
-  const { name, avatar, whatsapp, bio, subject, cost, schedule } = request.body;
-
-  const transaction = await database.transaction();
-
-  const insertedUsersIds = await transaction('users').insert({
-    name,
-    avatar,
-    whatsapp,
-    bio,
-  });
-
-  const user_id = insertedUsersIds[0];
-
-  try {
-    const insertedClassesIds = await transaction('classes').insert({
-      subject,
-      cost,
-      user_id,
-    });
-
-    const class_id = insertedClassesIds[0];
-
-    const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-      return {
-        class_id,
-        week_day: scheduleItem.week_day,
-        from: convertHoursToMinutes(scheduleItem.from),
-        to: convertHoursToMinutes(scheduleItem.to),
-      };
-    });
-
-    await transaction('class_schedule').insert(classSchedule);
-
-    // Final da Transaction
-    await transaction.commit();
-
-    return response.status(201).send();
-  } catch (err) {
-    // Caso tenha dado algum erro, qualquer alteracao no DB é desfeita
-    await transaction.rollback();
-
-    return response.status(400).json({
-      error: 'Unexpected error while creating new class.',
-    });
-  }
-});
+routes.get('/classes', classesController.index);
+routes.post('/classes', classesController.create);
 
 export default routes;

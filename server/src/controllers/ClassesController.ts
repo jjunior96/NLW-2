9ac47c12de23cf4ad91468481a169aa685fd18a6
Junior <jjalves96@gmail.com>
2020-08-05 15:02:20 -0300

    fix: ClassesController fix

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -23,18 +23,21 @@ export default class ClassesController {
         .json({ error: 'Missing filters to search classes.' });
     }
 
-    const timeInMinutes = convertHoursToMinutes(time);
+    const timeInMinutes = convertHoursToMinutes(filters.time as string);
 
     const classes = await database('classes')
       .whereExists(function () {
         this.select('class_schedule.*')
           .from('class_schedule')
           .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
-          .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+          .whereRaw('`class_schedule`.`week_day` = ??', [
+            Number(filters.week_day as string),
+          ])
           .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
-          .whereRaw('`class_schedule`.`from` > ??', [timeInMinutes]);
+          // Cuidado! "to", não "from"
+          .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes]);
       })
-      .where('classes.subject', '=', subject)
+      .where('classes.subject', '=', filters.subject as string)
       .join('users', 'classes.user_id', '=', 'users.id')
       .select(['classes.*', 'users.*']);
 

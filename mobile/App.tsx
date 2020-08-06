af64e30d929da93c6cf500b7e526be9a9f651c6b
Junior <jjalves96@gmail.com>
2020-08-06 10:25:57 -0300

    feat: Navigation Stack

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Landing from './src/pages/Landing';
 import { AppLoading } from 'expo';
 import {
   Archivo_400Regular,
@@ -12,6 +11,7 @@ import {
   Poppins_400Regular,
   Poppins_600SemiBold,
 } from '@expo-google-fonts/poppins';
+import AppStack from './src/routes/AppStack';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -26,7 +26,7 @@ export default function App() {
   } else {
     return (
       <>
-        <Landing />
+        <AppStack />
         <StatusBar style="light" />
       </>
     );

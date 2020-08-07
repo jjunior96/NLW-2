07a67a791a4d1ba4f6d0a12a819dc23d09afa4c9
Junior <jjalves96@gmail.com>
2020-08-07 09:41:56 -0300

    chore: add filters into TeacherList

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
 import { ScrollView } from 'react-native-gesture-handler';
@@ -9,7 +9,36 @@ import styles from './styles';
 function TeacherList() {
   return (
     <View style={styles.container}>
-      <PageHeader title="Proffys disponíveis" />
+      <PageHeader title="Proffys disponíveis">
+        <View style={styles.searchForm}>
+          <Text style={styles.label}>Matéria</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Qual a matéria?"
+            placeholderTextColor="#c1bcc"
+          />
+
+          <View style={styles.inputGroup}>
+            <View style={styles.inputBlock}>
+              <Text style={styles.label}>Dia da semana</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Qual o dia?"
+                placeholderTextColor=""
+              />
+            </View>
+
+            <View style={styles.inputBlock}>
+              <Text style={styles.label}>Horário</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Qual horário?"
+                placeholderTextColor=""
+              />
+            </View>
+          </View>
+        </View>
+      </PageHeader>
 
       <ScrollView
         style={styles.teacherList}
